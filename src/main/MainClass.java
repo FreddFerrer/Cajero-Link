fb2c53a63abf916fca4d218b7ea810039be42400
FreddyWhitDoubleD <ferreirafreddy39@gmail.com>
2021-08-23 22:55:34 -0300

    Logica CajeroLink

---
+++ src/main/MainClass.java
@@ -0,0 +1,12 @@
+package main;
+import funciones.*;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        Opciones mensajero_opciones = new Consulta();
+        mensajero_opciones.setSaldo(6513);
+        mensajero_opciones.operaciones();
+
+    }
+}
